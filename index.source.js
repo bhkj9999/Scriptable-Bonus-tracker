205850ac31366e68de4dc46b0ff8163e6a60ca06
HenryZ <hczhang39@icloud.com>
2023-10-06 12:27:22 -0400

    recover lost

--- index.source.js
+++ index.source.js
@@ -4,6 +4,12 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: code-branch;
+// 
+// 「小件件」
+// 开发环境，用于小组件调用
+// https://x.im3x.cn
+// https://github.com/im3x/Scriptables
+// 
 
 // 组件基础类
 const RUNTIME_VERSION = 20201209
